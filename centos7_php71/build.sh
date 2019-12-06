4a0fb5a1c5c7036d25591077546b72225cacc8f3
Ildar Bikmamatov <vistoyn@gmail.com>
2019-12-06 22:55:12 +0600

    centos7_php71 ready

--- centos7_php71/build.sh
+++ centos7_php71/build.sh
@@ -5,22 +5,18 @@ SCRIPT_PATH=`dirname $SCRIPT`
 BASE_PATH=`dirname $SCRIPT_PATH`
 
 RETVAL=0
+TAG=`date '+%Y%m%d_%H%M%S'`
 
 case "$1" in
 	
-	stage_packages)
-		docker build ./ -t bayrell/centos7_php71:stage_packages --file stages/01_stage_packages
-		cd ..
-	;;
-	
-	stage_etc)
-		docker build ./ -t bayrell/centos7_php71:stage_etc --file stages/02_stage_etc
-		docker tag bayrell/centos7_php71:stage_etc bayrell/centos7_php71:latest
+	docker)
+		docker build ./ -t bayrell/centos7_php71:$TAG --file stages/Dockerfile
+		docker tag bayrell/centos7_php71:$TAG bayrell/centos7_php71:latest
 		cd ..
 	;;
 	
 	*)
-		echo "Usage: $0 {stage_packages|stage_etc|final}"
+		echo "Usage: $0 {docker}"
 		RETVAL=1
 
 esac
